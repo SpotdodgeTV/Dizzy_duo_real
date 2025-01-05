132d5f781d264649d3a81366d833b29c63794541
VSoggyBurrito <112400006+VSoggyBurrito@users.noreply.github.com>
2025-01-04 23:58:59 -0600

    Player two sword
    Rotating sword that follows mouse position that plays a swing animation on left mouse click

--- scripts/player2.gd
+++ scripts/player2.gd
@@ -1,14 +1,22 @@
 extends RigidBody2D
 
+@onready var sword = $Sword
+@onready var sword_animation = $Sword/AnimationPlayer
+
 const MAGNITUDE = 100
+const SWORD_ROTATION_SPEED = 15
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+func _input(event: InputEvent) -> void:
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		sword_animation.play("Swing")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	sword.rotation = get_angle_to(get_local_mouse_position())
+	#sword.rotation = lerp_angle(sword.rotation, get_angle_to(get_local_mouse_position()), SWORD_ROTATION_SPEED * delta)
 
 func sling(direction, speed, nposition):
 	global_position = nposition
