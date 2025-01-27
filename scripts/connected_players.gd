b0544773fcf7a7e4b7b313e5d4e69c628b9ef785
SpotdodgeTV <nicomorin01@gmail.com>
2025-01-26 22:38:37 -0600

    eagonhu

--- scripts/connected_players.gd
+++ scripts/connected_players.gd
@@ -3,13 +3,14 @@ extends Node2D
 var health: int = 10
 var dead: bool = false
 var hearts_list : Array[TextureRect]
+@onready var player_healthbar = $"../CanvasLayer/PlayerHealth"
 
 func _ready() -> void:
 	pass
 
 func hurt(body, damage = 10):
 	health -= 1
-	$PlayerHealth2.update_hearts(health)
+	player_healthbar.update_hearts(health)
 	
 	if health <= 0:
 		dead = true
