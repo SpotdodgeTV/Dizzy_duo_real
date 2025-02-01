5b496f5e14eb086675572db992f20da80017e879
VSoggyBurrito <112400006+VSoggyBurrito@users.noreply.github.com>
2025-02-01 11:11:11 -0600

    game over screen

--- scenes/game_over.gd
+++ scenes/game_over.gd
@@ -1,4 +1,9 @@
 extends CanvasLayer
 
+@onready var scene_changer = get_tree().get_nodes_in_group("SceneChanger")[0]
+
 func _on_play_pressed() -> void:
-	$SceneChanger.change_scene(str(get_tree().current_scene))
+	scene_changer.change_scene(get_tree().current_scene.scene_file_path)
+
+func play_splat():
+	$AnimatedSprite2D.play("splat")
